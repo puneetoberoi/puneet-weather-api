0e3b0dca4743c6b4108a321151e8f557d41446aa
puneetoberoi <59065458+puneetoberoi@users.noreply.github.com>
2020-06-13 19:56:38 +0530

    Final for heroku

--- src/app.js
+++ src/app.js
@@ -68,23 +68,10 @@ app.get('/weather', (req, res) => {
     })
 })
 
-// app.get('/products', (req, res) => {
-//     if (!req.query.search) {
-//         return res.send({
-//             error: 'You must provide a search term'
-//         })
-//     }
-
-//     console.log(req.query.search)
-//     res.send({
-//         products: []
-//     })
-// })
-
 app.get('/help/*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Jaspreet Singh',
         errorMessage: 'Help article not found.'
     })
 })
@@ -92,7 +79,7 @@ app.get('/help/*', (req, res) => {
 app.get('*', (req, res) => {
     res.render('404', {
         title: '404',
-        name: 'Andrew Mead',
+        name: 'Jaspreet Singh',
         errorMessage: 'Page not found.'
     })
 })
